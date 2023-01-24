7c51e3877b9f0b88c48c4460dad1c7bf272207bc
zht1702 <zht1702@outlook.com>
2023-01-24 11:26:06 +0800

    Auto commit.

--- auto.bat
+++ auto.bat
@@ -2,6 +2,7 @@
 @title bat execute git auto commit #运行时命令行窗口的 title
 D:
 cd D:\wpssyncfolder\notebook #这里是要提交的项目目录，需要更改成自己的文件目录
+git status
 git add .
 git commit -m "Auto commit."
 git push #git 命令
